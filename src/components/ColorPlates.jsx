92961a2fd8aaee0e07cd7be158cdc777a3aeb765
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-03 14:03:03 -0700

    create html elements to style colored plates

--- src/components/ColorPlates.jsx
+++ src/components/ColorPlates.jsx
@@ -3,9 +3,30 @@ import React from 'react';
 function ColorPlates() {
   return(
     <div>
-      <p>color plates</p>
-    </div>
-  );
-}
+      <style jsx>{`
+          .outerPlate {
+            border: 1px solid red;
+            width: 100px;
+            height: 100px;
+          }
+          .innerPlate {
+            border: 1px dotted red;
+            width: 50%;
+            height: 50%;
+          }
 
-export default ColorPlates;
+          `}
+        </style>
+
+        <div>
+          <div className='outerPlate'>
+            <p className='innerPlate'></p>
+          </div>
+        </div>
+
+
+      </div>
+    );
+  }
+
+  export default ColorPlates;
