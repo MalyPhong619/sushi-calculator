94a9694ec81f70ddda9d7f3f52fc0c18440fee00
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-03 14:16:15 -0700

    center and round .plate > .outerPlate > .innerPlate

--- src/components/ColorPlates.jsx
+++ src/components/ColorPlates.jsx
@@ -4,21 +4,32 @@ function ColorPlates() {
   return(
     <div>
       <style jsx>{`
+          .plate {
+            border: 1px solid blue;
+            width: 25%;
+            text-align: center;
+          }
           .outerPlate {
+            display: inline-block;
             border: 1px solid red;
+            border-radius: 50%;
             width: 100px;
             height: 100px;
           }
           .innerPlate {
+            display: inline-block;
+            vertical-align: center;
             border: 1px dotted red;
+            border-radius: 50%;
             width: 50%;
             height: 50%;
+            margin-top: 25%;
           }
 
           `}
         </style>
 
-        <div>
+        <div className='plate'>
           <div className='outerPlate'>
             <p className='innerPlate'></p>
           </div>
