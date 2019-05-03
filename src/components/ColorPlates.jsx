fde3013a4f9b4a12edcea2f731b43546a61fab53
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-03 14:31:15 -0700

    create multiple colored plates in ColorPlates

--- src/components/ColorPlates.jsx
+++ src/components/ColorPlates.jsx
@@ -1,36 +1,56 @@
 import React from 'react';
 
 function ColorPlates() {
+
   return(
     <div>
       <style jsx>{`
           .plate {
-            border: 1px solid blue;
+            margin-top: 5px;
             width: 25%;
             text-align: center;
           }
           .outerPlate {
             display: inline-block;
-            border: 1px solid red;
+            border: 1px solid black;
             border-radius: 50%;
-            width: 100px;
-            height: 100px;
+            width: 200px;
+            height: 200px;
           }
           .innerPlate {
             display: inline-block;
             vertical-align: center;
-            border: 1px dotted red;
+            border: 1px dotted black;
             border-radius: 50%;
             width: 50%;
             height: 50%;
             margin-top: 25%;
+            background-color: white;
           }
-
           `}
         </style>
-
-        <div className='plate'>
-          <div className='outerPlate'>
+        <div className='plate redPlate'>
+          <div style={{backgroundColor: '#FF2A2B'}} className='outerPlate'>
+            <p className='innerPlate'></p>
+          </div>
+        </div>
+        <div className='plate bluePlate'>
+          <div style={{backgroundColor: '#0F97FF'}} className='outerPlate'>
+            <p className='innerPlate'></p>
+          </div>
+        </div>
+        <div className='plate greenPlate'>
+          <div style={{backgroundColor: '#54FF40'}} className='outerPlate'>
+            <p className='innerPlate'></p>
+          </div>
+        </div>
+        <div className='plate yellowPlate'>
+          <div style={{backgroundColor: '#B000FF'}} className='outerPlate'>
+            <p className='innerPlate'></p>
+          </div>
+        </div>
+        <div className='plate purplePlate'>
+          <div style={{backgroundColor: '#F5E922'}} className='outerPlate'>
             <p className='innerPlate'></p>
           </div>
         </div>
