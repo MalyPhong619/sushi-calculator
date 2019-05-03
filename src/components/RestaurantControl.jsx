a5f9fef36a6f44382188de3754f985377345274e
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-03 13:51:58 -0700

    create, import & export ColorPlates - display working

--- src/components/RestaurantControl.jsx
+++ src/components/RestaurantControl.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Restaurant from './Restaurant';
+import ColorPlates from './ColorPlates';
 
 function RestaurantControl() {
   return(
     <div>
       <p>This is Restaurant Control</p>
+      <ColorPlates />
       <Restaurant />
     </div>
   );
