f195b197471a70f63aaedfcb95cccdaa55a24a70
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-20 14:09:46 -0700

    move state to Display component - not working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,65 +8,65 @@ import { Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
-   super(props);
-   this.state = {
-     masterRestaurantList: [
-       {
-         name: 'Sushi Sakura',
-         location: '506 SW 6th Ave',
-         hoursOpen: '11',
-         hoursClosed: '9:30',
-         red: 1.2,
-         blue: 2.1,
-         yellow: 2.6,
-         orange: 3.1
-       },
-       {
-         name: 'Sushi Ohana',
-         location: '1422 Northeast Broadway',
-         hoursOpen: '11',
-         hoursClosed: '9:30',
-         red: 1.2,
-         blue: 2.1,
-         yellow: 2.6,
-         green: 3.1
-       },
-       {
-         name: 'Sushi Chiyo',
-         location: '4029 NE Sandy Blvd',
-         hoursOpen: '11',
-         hoursClosed: '9:30',
-         red: 1.2,
-         blue: 2.1,
-         yellow: 2.6,
-         purple: 3.1
-       },
-       {
-         name: 'Sushi Sapporo',
-         location: '9738 SE Washington St ste m',
-         hoursOpen: '11',
-         hoursClosed: '9:30',
-         red: 1.2,
-         blue: 2.1,
-         yellow: 2.6,
-         green: 3.1
-       }
-     ]
-   };
+    super(props);
+    this.state = {
+      masterRestaurantList: [
+        {
+          name: 'Sushi Sakura',
+          location: '506 SW 6th Ave',
+          hoursOpen: '11',
+          hoursClosed: '9:30',
+          red: 1.2,
+          blue: 2.1,
+          yellow: 2.6,
+          orange: 3.1
+        },
+        {
+          name: 'Sushi Ohana',
+          location: '1422 Northeast Broadway',
+          hoursOpen: '11',
+          hoursClosed: '9:30',
+          red: 1.2,
+          blue: 2.1,
+          yellow: 2.6,
+          green: 3.1
+        },
+        {
+          name: 'Sushi Chiyo',
+          location: '4029 NE Sandy Blvd',
+          hoursOpen: '11',
+          hoursClosed: '9:30',
+          red: 1.2,
+          blue: 2.1,
+          yellow: 2.6,
+          purple: 3.1
+        },
+        {
+          name: 'Sushi Sapporo',
+          location: '9738 SE Washington St ste m',
+          hoursOpen: '11',
+          hoursClosed: '9:30',
+          red: 1.2,
+          blue: 2.1,
+          yellow: 2.6,
+          green: 3.1
+        }
+      ]
+    };
 
 
-render(){
-  return (
-    <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={Display} />
-        <Route exact path='/calculator' component={CalcDisplay} />
-        <Route component={Error404}/>
-      </Switch>
-    </div>
-  );
-}
-}
+    render(){
+      return (
+        <div className="App">
+          <Header/>
+          <Switch>
+            <Route exact path='/'  render={()=><Display restaurantList={this.props.masterRestaurantList} />} />
+            <Route exact path='/calculator' component={CalcDisplay} />
+            <Route component={Error404}/>
+          </Switch>
+        </div>
+      );
+    }
+  }
 
-export default App;
+  export default App;
