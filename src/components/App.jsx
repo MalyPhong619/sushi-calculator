bf1657900601e86137291045e865afe437f8da4c
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-20 13:37:02 -0700

    add 4 sushi restaurants to state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,52 @@ import CalcDisplay from './CalcDisplay';
 import { Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     masterRestaurantList: [
+       {
+         name: 'Sushi Sakura',
+         location: '506 SW 6th Ave',
+         hoursOpen: '11',
+         hoursClosed: '9:30',
+         red: 1.2,
+         blue: 2.1,
+         yellow: 2.6,
+         orange: 3.1
+       },
+       {
+         name: 'Sushi Ohana',
+         location: '1422 Northeast Broadway',
+         hoursOpen: '11',
+         hoursClosed: '9:30',
+         red: 1.2,
+         blue: 2.1,
+         yellow: 2.6,
+         green: 3.1
+       },
+       {
+         name: 'Sushi Chiyo',
+         location: '4029 NE Sandy Blvd',
+         hoursOpen: '11',
+         hoursClosed: '9:30',
+         red: 1.2,
+         blue: 2.1,
+         yellow: 2.6,
+         purple: 3.1
+       },
+       {
+         name: 'Sushi Sapporo',
+         location: '9738 SE Washington St ste m',
+         hoursOpen: '11',
+         hoursClosed: '9:30',
+         red: 1.2,
+         blue: 2.1,
+         yellow: 2.6,
+         green: 3.1
+       }
+     ]
+   };
 
 
 render(){
