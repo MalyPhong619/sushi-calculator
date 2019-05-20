e1750bea75e1780c2fd6b6d8f817c37490870761
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-20 15:07:32 -0700

    create loop for restaurantList

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Restaurant from './Restaurants/Restaurant';
 
 function Display(props){
   console.log(props.restaurantList)
-  const Something = styled.h1`
-  @keyframes pulse {
-    0%{color: hotpink}
-    50%{color: limegreen}
-    100%{color: hotpink}
-  }
-    animation: pulse 1s linear infinite;
-    color: green;
-    &:hover {
-      cursor: pointer;
-      animation: none;
-      color: black;
-    }
-  `
+  
   return(
     <div>
-      <Something>Welcome to the home component</Something>
+      {props.restaurantList.map((post, index) =>
+        <Restaurant
+          month={post.name}
+          key={index} />
+      )}
     </div>
   )
 }
