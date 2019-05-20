fa53924d987965d3ed8becbf207488999d3bb63f
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-20 09:43:35 -0700

    set up propTypes for Restaurant

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -4,10 +4,19 @@ import PropTypes from 'prop-types';
 function Restaurant(props) {
   return(
     <div>
-      <p>This is restaurant component</p>
+      <h1>{props.name}</h1>
+      <h2>{props.location}</h2>
+      <p>{props.hoursOpen} / {props.hoursClosed}</p>
     </div>
 
   );
+}
+
+Restaurant.propTyepes = {
+  name: PropTypes.string,
+  location: PropTypes.string,
+  hoursOpen: PropTypes.string,
+  hoursClosed: PropTypes.string
 }
 
 export default Restaurant;
