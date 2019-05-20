4ba6b6ef439a6a572ac7e559e54fd63d459a414a
Maly Phongsavanh <m.phongsavanh619@gmail.com>
2019-05-20 09:34:12 -0700

    create new path 'calculator' and render ColorPlates'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,18 @@ import './App.scss';
 import Error404 from './Error404';
 import Header from './Header';
 import Display from './Display';
+import ColorPlates from './ColorPlates';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
 
-  
+
   return (
     <div className="App">
       <Header/>
       <Switch>
         <Route exact path='/' component={Display} />
+        <Route exact path='/calculator' component={ColorPlates} />
         <Route component={Error404}/>
       </Switch>
     </div>
